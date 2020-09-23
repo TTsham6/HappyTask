f3059b3a7f427ff33442dfb3d9574e9bfd73562a
ttsham6 <troots@hotmail.co.jp>
2020-09-23 13:42:39 +0900

    heroku実行環境用にDB設定を変更した。

--- we-back/app_go/db/db.go
+++ we-back/app_go/db/db.go
@@ -1,7 +1,10 @@
 package db
 
 import (
+	"os"
+	
 	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
 	_ "github.com/jinzhu/gorm/dialects/postgres" // Use PostgreSQL in gorm
 )
 
@@ -11,13 +14,32 @@ var (
 )
 
 // Init DBの初期設定を行う
-func Init() {
+// func Init() {
+// 	// TODO 設定ファイルからの読み込みに変更する
+// 	db, err = gorm.Open("postgres", "host=0.0.0.0 port=5432 user=we-user dbname=wefundb password=we-admin sslmode=disable")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+// Init DBの初期設定を行う(HEROKUのPostgreSQLアドオン用の設定)
+func Init(){
 	// TODO 設定ファイルからの読み込みに変更する
-	db, err = gorm.Open("postgres", "host=0.0.0.0 port=5432 user=we-user dbname=wefundb password=we-admin sslmode=disable")
+	url:=os.Getenv("DATABASE_URL")
+	connection,err:=pq.ParseURL(url)
+	if err!=nil{
+		panic(err.Error())
+	}
+	connection += " sslmode=require"
+
+	db,err =gorm.Open("postgres",connection)
 	if err != nil {
-		panic(err)
+		panic(err.Error())
 	}
 }
+
+
+
 
 // GetDB モデルを呼び出す
 func GetDB() *gorm.DB {
